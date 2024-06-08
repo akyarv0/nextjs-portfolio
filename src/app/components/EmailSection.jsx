0acc22503fad82ff0b9237343149de2fb670df95
akyarv0 <akyarv0@gmail.com>
2024-06-09 02:26:46 +0300

    publickey

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,13 +1,48 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
-
 const EmailSection = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    subject: '',
+    message: ''
+  });
+  
+  const [emailSent, setEmailSent] = useState(false);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.send(
+      'service_6gm3doe',  // Service ID'nizi buraya ekleyin
+      'template_mr3foub', // Template ID'nizi buraya ekleyin
+      formData,
+      'l5KWPLiN2fYrAgq41'      // User ID'nizi buraya ekleyin
+    ).then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      setEmailSent(true);
+      setFormData({
+        email: '',
+        subject: '',
+        message: ''
+      });
+    }).catch((error) => {
+      console.log('FAILED...', error);
+    });
+  };
 
   return (
     <section
@@ -20,7 +55,6 @@ const EmailSection = () => {
           Let&apos;s Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
@@ -31,69 +65,76 @@ const EmailSection = () => {
           </Link>
           <Link href="https://www.linkedin.com/in/veysel-akyar/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>        </div>
+          </Link>        
+        </div>
       </div>
       <div className="z-10 mt-8">
-        
-          {/* <p className="text-green-500 text-sm mt-2">
+        {emailSent && (
+          <p className="text-green-500 text-sm mt-2">
             Email sent successfully!
-          </p> */}
-       
-          <form className="flex flex-col" >
-            <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="text-white block mb-2 text-sm font-medium"
-              >
-                Your email
-              </label>
-              <input
-                name="email"
-                type="email"
-                id="email"
-                required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="yourEmail@example.com"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="subject"
-                className="text-white block text-sm mb-2 font-medium"
-              >
-                Subject
-              </label>
-              <input
-                name="subject"
-                type="text"
-                id="subject"
-                required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="message"
-                className="text-white block text-sm mb-2 font-medium"
-              >
-                Message
-              </label>
-              <textarea
-                name="message"
-                id="message"
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Let's talk about..."
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          </p>
+        )}
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="text-white block mb-2 text-sm font-medium"
             >
-              Send Message
-            </button>
-          </form>
-        
+              Your email
+            </label>
+            <input
+              name="email"
+              type="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="yourEmail@example.com"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="subject"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Subject
+            </label>
+            <input
+              name="subject"
+              type="text"
+              id="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Just saying hi"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              value={formData.message}
+              onChange={handleChange}
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Let's talk about..."
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          >
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
